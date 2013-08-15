83e3be0d3f1a93ccf0f5f0895eeede60f36b72af
dezoxel <dezoxel@gmail.com>
2013-08-15 09:27:32 +0300

    Spec titles has been written

--- test/router.spec.js
+++ test/router.spec.js
@@ -1,8 +1,11 @@
 define(['router'], function(router) {
 
   describe('Router', function() {
-    it('dummy', function() {
-      expect(true).toBe(true);
-    });
+
+    it('resets all routing maps');
+    it('maps route pattern to the module name');
+    it('supports params in the route pattern');
+    it('requires "module" option when defining a route');
+    it('moves control to the callback after the route matched');
   });
 });
