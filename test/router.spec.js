7df201f0a557814d161e2bec8a6d126593bd8aee
dezoxel <dezoxel@gmail.com>
2013-08-16 22:43:19 +0300

    Test for route registering has been written. Restructured code

--- test/router.spec.js
+++ test/router.spec.js
@@ -2,21 +2,34 @@ define(['router'], function(router) {
 
   describe('Router', function() {
 
-    it('doesnt have any routes when loaded', function() {
-      expect(router.getAllRoutes()).toEqual([]);
-    });
-
-    it('resets all routes', function() {
-      router.when('hello/world', {module: 'hello-world'});
-
+    beforeEach(function() {
       router.reset();
-
-      expect(router.getAllRoutes()).toEqual([]);
     });
 
-    it('maps route pattern to the module name');
+    it('registers route pattern and maps it to the module name', function() {
+      router.when('user/:id/profile', {module: 'user-profile'});
+
+      expect(router.route('user/:id/profile').module).toEqual('user-profile')
+    });
+
     it('supports params in the route pattern');
     it('requires "module" option when defining a route');
     it('moves control to the callback after the route matched');
+
+    describe('when empty', function() {
+
+      it('doesnt have any routes when loaded', function() {
+        expect(router.hasAnyRoutes()).toBe(false);
+      });
+
+      it('resets all routes', function() {
+        router.when('hello/world', {module: 'hello-world'});
+
+        router.reset();
+
+        expect(router.hasAnyRoutes()).toBe(false);
+      });
+    });
+
   });
 });
