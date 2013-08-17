13e49b16625b0ba0dd2e900e259fbf0bbe692790
dezoxel <dezoxel@gmail.com>
2013-08-17 19:59:58 +0300

    Added support for default route options

--- test/router.spec.js
+++ test/router.spec.js
@@ -2,30 +2,36 @@ define(['router'], function(router) {
 
   describe('Router', function() {
 
-    beforeEach(function() {
-      router.reset();
-    });
+    describe('when define the route', function() {
 
-    it('registers route pattern and maps it to the module name', function() {
-      router.when('user/:id/profile', {module: 'user-profile'});
+      beforeEach(function() {
+        router.reset();
 
-      expect(router.route('user/:id/profile').module).toEqual('user-profile')
-    });
-
-    it('supports params in the route pattern', function() {
-      router.when('news/:id', {module: 'news-details'})
-
-      router.bind('routeMatched', function(params) {
-        expect(params.id).toBe(2);
+        router.when('user/:id/profile', {module: 'user/profile'});
       });
 
-      goTo({route: 'news/:id', id: 2});
+      it('registers route pattern and maps it to the module name', function() {
+        expect(router.route('user/:id/profile').module).toEqual('user/profile')
+      });
+
+      it('supports params in the route pattern', function() {
+        router.bind('routeMatched', function(params) {
+          expect(params.id).toBe(2);
+        });
+
+        goTo({route: 'user/:id/profile', id: 2});
+      });
+
+      it('passes custom route params when defining a route');
+
+      it('moves control to the callback after the route matched');
     });
 
-    it('requires "module" option when defining a route');
-    it('moves control to the callback after the route matched');
-
     describe('when empty', function() {
+
+      beforeEach(function() {
+        router.reset();
+      });
 
       it('doesnt have any routes when loaded', function() {
         expect(router.hasAnyRoutes()).toBe(false);
