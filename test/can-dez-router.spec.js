164359b5f64059cea9cc18d72405801d295fd9b9
dezoxel <dezoxel@gmail.com>
2013-09-07 19:48:52 -0700

    Fixed error when route wasnt triggering the routeChanged event if route param changed

--- test/can-dez-router.spec.js
+++ test/can-dez-router.spec.js
@@ -35,6 +35,21 @@ define(['can-dez-router', 'can/route'], function(router, CanRoute) {
       goTo({route: 'hello/world'});
     });
 
+    it('changes route even if param changed in the same route', function() {
+      var id = 0;
+      router.bind('routeMatched', function(params) {
+        id = params.id;
+      });
+
+      expect(id).toEqual(0);
+
+      goTo({route: 'user/:id/profile', id: 2});
+      expect(id).toEqual(2);
+
+      goTo({route: 'user/:id/profile', id: 4});
+      expect(id).toEqual(4);
+    });
+
   });
 
   function goTo(params) {
